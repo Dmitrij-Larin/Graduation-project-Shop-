8690324160d78fa895652366e354e14d3b97aaa9
Dmitrij Larin <larin4921@gmail.com>
2025-06-18 11:35:35 +0300

    Исправлена ошибка.

--- orders/admin.py
+++ orders/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from orders.models import Order, OrderItems
+from orders.models import Order, OrderItem
 
 
 class OrderItemInline(admin.TabularInline):
-    model = OrderItems
+    model = OrderItem
     raw_id_fields = ['product']
 
 
