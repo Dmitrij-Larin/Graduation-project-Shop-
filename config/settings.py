0d9462b2c35b329a8bcf3798504d9df3cfeeda46
Dmitrij Larin <larin4921@gmail.com>
2025-06-28 22:11:54 +0300

    Добавлен платёжный шлюз в проект.

--- config/settings.py
+++ config/settings.py
@@ -165,3 +165,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CART_SESSION_ID = 'cart'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+STRIPE_API_VERSION = os.getenv('STRIPE_API_VERSION')
