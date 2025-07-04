36709b999120bfeaac2ad1bc081b1f4341a2421b
Dmitrij Larin <larin4921@gmail.com>
2025-07-05 02:32:24 +0300

    Добавлена логика отправки эл. письма пользователю.

---
+++ payment/tasks.py
@@ -0,0 +1,38 @@
+from io import BytesIO
+import weasyprint
+from celery import shared_task
+from django.conf import settings
+from django.contrib.staticfiles import finders
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from orders.models import Order
+
+from pathlib import Path
+
+
+@shared_task
+def payment_completed(order_id):
+    """
+    Задание по отправке уведомления по эл. почте при успешной оплате заказа.
+    """
+    order = Order.objects.get(id=order_id)
+    subject = f'Инструменты - Счёт-фактура №{order.id}'
+    message = 'Пожалуйста, ознакомьтесь с приложенной счёт-фактурой за Вашу недавнюю покупку.'
+    email = EmailMessage(
+        subject, message, 'admin@web.top', [order.email]
+    )
+    # сгенерировать PDF
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    out = BytesIO()
+    stylesheets=[weasyprint.CSS(Path(settings.STATIC_ROOT) / 'css/pdf.css')]
+    weasyprint.HTML(string=html).write_pdf(
+        out, stylesheets=stylesheets
+    )
+    # прикрепить PDF-файл
+    email.attach(
+        f'Заказ {order.id}.pdf',
+        out.getvalue(),
+        'application/pdf'
+    )
+    # отправить электронное письмо
+    email.send()
--- payment/webhooks.py
+++ payment/webhooks.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from orders.models import Order
-# from payment.views import payment_completed
+from payment.tasks import payment_completed
 
 
 @csrf_exempt
@@ -37,7 +37,7 @@ def stripe_webhook(request):
             # сохранить id платежа Stripe
             order.stripe_id = session.payment_intent
             order.save()
-            # # запустить асинхронное задание
-            # payment_completed.delay(order.id)
+            # запустить асинхронное задание
+            payment_completed.delay(order.id)
 
     return HttpResponse(status=200)
